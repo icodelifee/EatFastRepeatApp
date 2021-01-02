2c9b463ae5cb94b49f6114bd2c1013f4c86c1af5
icodelifee <mithunsmenon@gmail.com>
2021-01-02 13:38:58 +0530

    ♻️ Refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
+    MaterialApp(),
+  );
+}
+
+class MaterialApp extends StatelessWidget {
+  const MaterialApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          backgroundColor: Colors.black,
+          primaryColor: Color(0xFFFFBB92),
+          buttonColor: Color(0xFF2E2E2E),
+          accentColor: Color(0xFFFFBB92)),
       title: "Eat Fast Repeat",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: AppPages.LOGIN,
       getPages: AppPages.routes,
       defaultTransition: Transition.cupertino,
-    ),
-  );
+    );
+  }
 }
