56a102352fd9e2f6e28b88ad20cd6fc5135371e2
icodelifee <mithunsmenon@gmail.com>
2021-01-02 13:42:42 +0530

    ✨ Generate pages using get_cli

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,18 +1,26 @@
 import 'package:get/get.dart';
 
 import 'package:eatfastrepeat/app/modules/home/bindings/home_binding.dart';
-import 'package:eatfastrepeat/app/modules/home/views/home_view.dart';
+import 'package:eatfastrepeat/app/modules/home/views/home.view.dart';
+import 'package:eatfastrepeat/app/modules/login/bindings/login_binding.dart';
+import 'package:eatfastrepeat/app/modules/login/views/login.view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   static const INITIAL = Routes.HOME;
+  static const LOGIN = Routes.LOGIN;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
